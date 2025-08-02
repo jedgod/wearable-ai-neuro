82c2b8baac794717882a99e3b8996a9b536f7f5b
Jed Jerry <67393833+jedgod@users.noreply.github.com>
2025-08-01 23:19:57 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+#import plotly.express as px
+
 from sklearn.ensemble import GradientBoostingClassifier
 
 # Streamlit Dashboard for Wearable AI Analysis
